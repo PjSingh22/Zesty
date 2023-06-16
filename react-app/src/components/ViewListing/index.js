07d9f62b56deb4ab5e22ffb297d36f069e646b72
Prabhjot Singh <pjsingh1359@gmail.com>
2023-06-16 10:14:36 -0700

    got carousel working

--- react-app/src/components/ViewListing/index.js
+++ react-app/src/components/ViewListing/index.js
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getSingleListingThunk } from "../../store/listings";
-
-
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import "./viewlisting.css"
 function ViewListing() {
   const { id } = useParams()
   const listing = useSelector(state => state.listings.singleListing);
+  // const images = useSelector(state => state.listings.singleListing.images)
   const dispatch = useDispatch();
 
   console.log("view listing", listing)
@@ -16,8 +17,28 @@ function ViewListing() {
 
   if (!listing) return null
   return (
-    <div>
-      <h1>view listing {listing.name}</h1>
+    <div className="view-listing-container">
+      <div className="view-listing__carousel">
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          totalSlides={listing?.images?.length}
+          visibleSlides={1}
+          step={1}
+          infinite
+          isIntrinsicHeight
+        >
+          <Slider>
+            {listing?.images?.map((image, i) => {
+              return <Slide index={i}><div className="image-slide" style={{backgroundImage: `url(${image.imageUrl})`, backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center", width: "500px", height: "200px"}}></div></Slide>
+            })}
+          </Slider>
+          <div className="carousel-navigation">
+            <ButtonBack className="img-btn">{`<`}</ButtonBack>
+            <ButtonNext className="img-btn">{`>`}</ButtonNext>
+          </div>
+        </CarouselProvider>
+      </div>
     </div>
   )
 }
