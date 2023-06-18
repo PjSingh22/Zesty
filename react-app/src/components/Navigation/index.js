4ad19847d87b2da26e09f347508cb049f6090084
Prabhjot Singh <pjsingh1359@gmail.com>
2023-06-18 16:33:23 -0700

    got content for navbar down

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
+
 function Navigation({ isLoaded }){
   const history = useHistory()
 	const sessionUser = useSelector(state => state.session.user);
@@ -11,17 +12,30 @@ function Navigation({ isLoaded }){
 	return (
 		<div className='nav-bar'>
 			<div>
-				<NavLink exact to="/">Home</NavLink>
+				<NavLink exact to="/"><div className='logo'></div></NavLink>
 			</div>
+      <div>
+        <form>
+          <label>
+            <input type='search'></input>
+          </label>
+        </form>
+      </div>
 			{isLoaded && (
-        <>
-        <div>
-          {sessionUser && <button onClick={() => history.push('/listings/new')}>Create Post</button>}
-        </div>
+        <div className='nav-right-side'>
+          <div>
+            {sessionUser && <button onClick={() => history.push('/listings/new')}>Create Listing</button>}
+          </div>
+          <div className='favs'>
+          <i className="far fa-heart fa-lg"></i>
+          </div>
           <div>
             <ProfileButton user={sessionUser} />
           </div>
-        </>
+          <div>
+            <i className="fas fa-shopping-cart fa-lg"></i>
+          </div>
+        </div>
 			)}
 		</div>
 	);
