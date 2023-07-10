3e2ed3cbdad59af97b50cfe63135389b5a140480
Prabhjot Singh <pjsingh1359@gmail.com>
2023-07-10 15:49:51 -0700

    test

--- react-app/src/components/CheckoutPage/index.js
+++ react-app/src/components/CheckoutPage/index.js
@@ -49,6 +49,7 @@ function CheckoutPage() {
   return (
     <div className="checkout-container">
       <div className="cart-items">
+        <p>test</p>
         <p className="cart-amount">{cartItems.length} {cartItems.length === 1? "item" : "items"} in your cart.</p>
         <button onClick={clearCart} className="clear-cart">Clear Cart</button>
         {cartItems?.map(item => {
